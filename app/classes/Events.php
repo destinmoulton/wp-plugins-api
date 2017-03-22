573192eb053bf06285ec051310bc60b05f6c049a
Destin Moulton <destin@gizmoproductions.com>
2017-03-22 18:42:30 -0400

    Removed logger messages from Events class.

--- app/classes/Events.php
+++ app/classes/Events.php
@@ -8,13 +8,11 @@ class Events{
     }
 
     function getDateRange($start_date, $end_date){
-        $this->logger->addInfo($end_date);
         $events = $this->db->ltdbsem_events()
                             ->where("event_start_date >= ? AND event_end_date <= ?", $start_date, $end_date)
                             ->order("event_id DESC")
                             ->limit(30);
 
-        $this->logger->addInfo((string) $events);
         $rows = [];
         foreach ($events as $event){
             $rows[] = $event;
