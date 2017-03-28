9095fd0aa0f225070158bcf466bb251d5b483c63
Destin Moulton <destin.moulton@gmail.com>
2017-03-28 14:57:46 -0600

    Added 'events' array key to json.

--- app/routes/events.php
+++ app/routes/events.php
@@ -7,7 +7,7 @@ $app->get('/events/{start_date}/{end_date}', function (Request $request, Respons
     $name = $request->getAttribute('name');
     $events = new Events($this->db, $this->logger);
     $evs = $events->getDateRange($args['start_date'], $args['end_date']);
-    $newResponse = $response->withJson($evs);
+    $newResponse = $response->withJson(['events'=>$evs]);
 
     return $newResponse;
 });
