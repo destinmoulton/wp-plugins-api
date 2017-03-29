ea6afeb0728b3f09c5fe37ce49d6f7dd19e63519
Destin Moulton <destin.moulton@gmail.com>
2017-03-29 10:05:46 -0600

    Added Locations function to cache the locations.

--- app/classes/Locations.php
+++ app/classes/Locations.php
@@ -1,5 +1,21 @@
 <?php
 
 class Locations {
-    
+    function __construct($db, $logger){
+        $this->db = $db;
+        $this->logger = $logger;
+
+        $this->locations = [];
+    }
+
+    function getCacheLocations(){
+        $locations = $this->db->ltdbsem_locations()
+                              ->order("location_id ASC");
+
+        $rows = [];
+        foreach ($locations as $loc){
+            $rows[] = $loc;
+        }
+        $this->locations = $rows;
+    }
 }
