bc8b2ff3c091b3f4c303133922742c99abae72a5
Destin Moulton <destin.moulton@gmail.com>
2017-03-30 12:06:21 -0600

    Added post meta query to get the image from the correct location.

--- app/classes/Images.php
+++ app/classes/Images.php
@@ -7,12 +7,19 @@ class Images {
     }
 
     function getImageForEvent($post_id){
-        $attachment = $this->db->ltdbsposts()
-                              ->where("post_parent", $post_id)
-                              ->where("post_status = 'inherit'")
-                              ->where("post_type = 'attachment'")
+        //ltdbspostmeta _thumbnail_id
+        $meta = $this->db->ltdbspostmeta()
+                         ->where("post_id", $post_id)
+                         ->where("meta_key", "_thumbnail_id")
+                         ->limit(1);
+        if(!isset($meta[0])){
+            return "";
+        }
+
+        $post = $this->db->ltdbsposts()
+                              ->where("ID", $meta[0]['meta_value'])
                               ->limit(1);
 
-        return ($attachment)? $attachment->guid : "";
+        return (isset($post[0]))? $post[0]['guid'] : "";
     }
 }
