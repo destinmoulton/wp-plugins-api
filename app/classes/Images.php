38c2b10ec0f04728f1091aa57179923b6329eaf5
Destin Moulton <destin.moulton@gmail.com>
2017-03-30 11:37:15 -0600

    Added query to getImageForEvent in Images class.

--- app/classes/Images.php
+++ app/classes/Images.php
@@ -7,6 +7,12 @@ class Images {
     }
 
     function getImageForEvent($post_id){
-        
+        $attachment = $this->db->ltdbsposts()
+                              ->where("post_parent", $post_id)
+                              ->where("post_status = 'inherit'")
+                              ->where("post_type = 'attachment'")
+                              ->limit(1);
+
+        return ($attachment)? $attachment->guid : "";
     }
 }
