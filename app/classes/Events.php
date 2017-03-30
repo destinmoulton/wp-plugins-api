90f56b6669a8b947d0f871ae9c3f3b8466df51bf
Destin Moulton <destin.moulton@gmail.com>
2017-03-30 11:38:41 -0600

    Added image collation to the getDateRange function.

--- app/classes/Events.php
+++ app/classes/Events.php
@@ -1,6 +1,7 @@
 <?php
 
 require('Locations.php');
+require('Images.php');
 
 class Events{
     function __construct($db, $logger){
@@ -11,6 +12,7 @@ class Events{
 
     function getDateRange($date){
         $locations = new Locations($this->db, $this->logger);
+        $images = new Images($this->db, $this->logger);
 
         $events = $this->db->ltdbsem_events()
                             ->where("event_start_date = ?", $date)
@@ -24,6 +26,8 @@ class Events{
             // Add the location data to the array
             $evData['location'] = $locations->getLocation($event['location_id']);
 
+            // Add the attached image to the array
+            $evData['image_url'] = $images->getImageForEvent($event['post_id']);
             $rows[] = $evData;
         }
         return $rows;
