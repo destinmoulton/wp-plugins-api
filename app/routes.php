2bac66afb21fccff03af61cae33bfe5a303c93c8
Destin Moulton <destin.moulton@gmail.com>
2017-04-17 12:03:35 -0600

    Modified the router to look in plugin directories.

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,8 @@
 <?php
+$PLUGIN_DIR = __DIR__ . '/plugins/';
 
-require('routes/events.php');
+$plugin_directories = glob($PLUGIN_DIR . '/*' , GLOB_ONLYDIR);
+
+foreach( $plugin_directories as $dir){
+    require($PLUGIN_DIR . $dir . '/routes.php');
+}
