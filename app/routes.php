3300d066d52495108957ec441d82777ed7f22e84
Destin Moulton <destin.moulton@gmail.com>
2017-04-17 12:09:56 -0600

    Removed the repetitive PLUGIN_DIR from the plugin path.

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,8 @@
 <?php
-$PLUGIN_DIR = realpath(__DIR__ . '../plugins/');
+$PLUGIN_DIR = realpath(__DIR__ . '/../plugins/');
 
 $plugin_directories = glob($PLUGIN_DIR . '/*' , GLOB_ONLYDIR);
 
-foreach( $plugin_directories as $dir){
-    require($PLUGIN_DIR . $dir . '/routes.php');
+foreach( $plugin_directories as $plugin_dir){
+    require($plugin_dir . '/routes.php');
 }
