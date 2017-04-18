a827a5f80c7fd697ded2de5e859b8cc0576c66ab
Destin Moulton <destin.moulton@gmail.com>
2017-04-18 16:16:39 -0600

    Added JwtAuthentication to the container configuration.

--- app/init.php
+++ app/init.php
@@ -8,17 +8,19 @@ $app = new \Slim\App(["settings" => $config]);
 // Use NotORM for the database 
 require 'lib/NotORM.php';
 
+require 'Token.php';
+
 $container = $app->getContainer();
 
-$container['logger'] = function($ctxt) {
+$container['logger'] = function($container) {
     $logger = new \Monolog\Logger('api_logger');
     $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
     $logger->pushHandler($file_handler);
     return $logger;
 };
 
-$container['db'] = function ($ctxt) {
-    $db = $ctxt['settings']['db'];
+$container['db'] = function ($container) {
+    $db = $container['settings']['db'];
     $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
         $db['user'], $db['pass'],array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES '" . $db['charset'] . "'"));
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -26,6 +28,34 @@ $container['db'] = function ($ctxt) {
     $db = new NotORM($pdo);
     return $db;
 };
+
+$container["token"] = function ($container) {
+    return new Token;
+};
+
+// Setup the JWT Middleware
+// https://www.appelsiini.net/projects/slim-jwt-auth
+$container["JwtAuthentication"] = function ($container) {
+    return new JwtAuthentication([
+        "path" => "/",
+        "passthrough" => ["/token"],
+        "secret" => $container['settings']['jwt']['secret'],
+        "logger" => $container["logger"],
+        "relaxed" => ["192.168.50.52"],
+        "error" => function ($request, $response, $arguments) {
+            $data["status"] = "error";
+            $data["message"] = $arguments["message"];
+            return $response
+                ->withHeader("Content-Type", "application/json")
+                ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        },
+        "callback" => function ($request, $response, $arguments) use ($container) {
+            $container["token"]->hydrate($arguments["decoded"]);
+        }
+    ]);
+};
+
+$app->add("JwtAuthentication");
 
 require('routes.php');
 
