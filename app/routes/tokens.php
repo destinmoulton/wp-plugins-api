a110a094e53251653cb76a2407a3a9d0d203fb5c
Destin Moulton <destin.moulton@gmail.com>
2017-04-18 16:28:56 -0600

    Created a new token route.

---
+++ app/routes/tokens.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Setup the JWT token route.
+ * @author Destin Moulton
+ * Modified From: 
+ *    https://github.com/tuupola/slim-api-skeleton/blob/master/routes/token.php
+ */
+$app->post("/token", function ($request, $response, $arguments) {
+   
+    $now = new DateTime();
+    $future = new DateTime("now +2 years");
+    $server = $request->getServerParams();
+    $jti = (new Base62)->encode(random_bytes(16));
+    $payload = [
+        "iat" => $now->getTimeStamp(),
+        "exp" => $future->getTimeStamp(),
+        "jti" => $jti,
+        "sub" => $server["PHP_AUTH_USER"]
+    ];
+    $secret = getenv("JWT_SECRET");
+    $token = JWT::encode($payload, $secret, "HS256");
+    $data["token"] = $token;
+    $data["expires"] = $future->getTimeStamp();
+    return $response->withStatus(201)
+        ->withHeader("Content-Type", "application/json")
+        ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+});
