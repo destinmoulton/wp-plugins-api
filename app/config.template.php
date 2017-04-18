00aa83a9ab2bef5bb66b4406a883cfb20db4c7c3
Destin Moulton <destin.moulton@gmail.com>
2017-04-18 15:48:52 -0600

    Added jwt secret to the config template.

--- app/config.template.php
+++ app/config.template.php
@@ -10,3 +10,6 @@ $config['db']['user']   = "username";
 $config['db']['pass']   = "password";
 $config['db']['dbname'] = "wordpress_database";
 $config['db']['charset'] = "utf8";
+
+// JWT Authentication Secret
+$config['jwt']['secret'] = "S!MVPO9w@B7UiARDG#ngdhLqdlF5r!HBV*pvoGZiZKY9BLHFmA3VjLTV!125t3s";
