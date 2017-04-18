50c5f37daaa28adf16eb6a03ae05fc703bf2218e
Destin Moulton <destin.moulton@gmail.com>
2017-04-18 16:16:19 -0600

    Added a Token class to handle the jwt tokens.

---
+++ app/Token.php
@@ -0,0 +1,14 @@
+<?php
+
+class Token
+{
+    public $decoded;
+    public function hydrate($decoded)
+    {
+        $this->decoded = $decoded;
+    }
+    public function hasScope(array $scope)
+    {
+        return !!count(array_intersect($scope, $this->decoded->scope));
+    }
+}
