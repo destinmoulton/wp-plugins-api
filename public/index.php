cd13d9da17b43f99164e7100a46ccf7e0c222896
Destin Moulton <destin.moulton@gmail.com>
2017-04-21 09:45:05 -0600

    Added CORS whitelist for localhost developement.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,15 @@
 <?php
 
 
+// Allow localhost access to the api
+$cors_whitelist = array('127.0.0.1','localhost');
+
+if(in_array($_SERVER['REMOTE_ADDR'], $cors_whitelist)){
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+}
+
 error_reporting(E_ALL);
 ini_set("display_errors", "on");
 
