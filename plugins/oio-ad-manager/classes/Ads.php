4e7e76b93b20af66326a955729a4fc1915c8e09a
Destin Moulton <destin.moulton@gmail.com>
2017-04-24 15:53:52 -0600

    Stubbed in function body to getAdsByType.

--- plugins/oio-ad-manager/classes/Ads.php
+++ plugins/oio-ad-manager/classes/Ads.php
@@ -6,5 +6,10 @@
  */
 
 class Ads {
-    
+    function getAdsByType($adType){
+        //The types of ads
+        $types = array( 'link' => 2, 'inline' => 3, 'banner' => 5 );
+
+        $sql = "SELECT * FROM " . $this->settings->dbtable_purchases . " WHERE item_channel=" . $types[$type] . " AND item_status=1 AND payment_status=1" . ($zones ? " AND item_type IN(" . implode(",", $zones) . ")" : "") . " AND (payment_time=0 OR payment_time < " . time() . ")" . $sql_extra . " ORDER BY payment_time DESC";
+    }
 }
