92a810131e747ea432d86adbafc07a184896b7ef
Destin Moulton <destin.moulton@gmail.com>
2017-04-24 17:42:14 -0600

    Rebuilt the Ads query into PDO format.

--- plugins/oio-ad-manager/classes/Ads.php
+++ plugins/oio-ad-manager/classes/Ads.php
@@ -18,7 +18,18 @@ class Ads {
         //The types of ads
         $types = array( 'link' => 2, 'inline' => 3, 'banner' => 5 );
 
+        $select = $this->db->select()
+                           ->from($this->settings['db']['prefix'] . self::OIO_TABLE_PURCHASES)
+                           ->where("item_channel", "=", $types[$type])
+                           ->where("item_status", "=", 1)
+                           ->where("payment_status", "=", 1)
+                           ->where("payment_time", "=", 0)
+                           ->orWhere("payment_time", "<", time())
+                           ->orderBy("payment_time", "DESC");
 
-        $sql = "SELECT * FROM " . self::OIO_TABLE_PURCHASES . " WHERE item_channel=" . $types[$type] . " AND item_status=1 AND payment_status=1" . ($zones ? " AND item_type IN(" . implode(",", $zones) . ")" : "") . " AND (payment_time=0 OR payment_time < " . time() . ")" . $sql_extra . " ORDER BY payment_time DESC";
+        $stmt = $select->execute();
+        return $stmt->fetchAll();
+
+        //$sql = "SELECT * FROM " . self::OIO_TABLE_PURCHASES . " WHERE item_channel=" . $types[$type] . " AND item_status=1 AND payment_status=1" . ($zones ? " AND item_type IN(" . implode(",", $zones) . ")" : "") . " AND (payment_time=0 OR payment_time < " . time() . ")" . $sql_extra . " ORDER BY payment_time DESC";
     }
 }
