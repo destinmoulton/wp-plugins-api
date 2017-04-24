b0d11c829bdf4546f012dae8419976a5b5771056
Destin Moulton <destin.moulton@gmail.com>
2017-04-24 17:27:33 -0600

    Added settings to the Ads constructor.

--- plugins/oio-ad-manager/classes/Ads.php
+++ plugins/oio-ad-manager/classes/Ads.php
@@ -8,14 +8,16 @@
 class Ads {
     const OIO_TABLE_PURCHASES = "ltdbsoiopub_purchases";
 
-    function __construct($db, $logger){
+    function __construct($db, $logger, $settings){
         $this->db = $db;
         $this->logger = $logger;
+        $this->settings = $settings;
     }
 
     function getAdsByType($adType){
         //The types of ads
         $types = array( 'link' => 2, 'inline' => 3, 'banner' => 5 );
+
 
         $sql = "SELECT * FROM " . self::OIO_TABLE_PURCHASES . " WHERE item_channel=" . $types[$type] . " AND item_status=1 AND payment_status=1" . ($zones ? " AND item_type IN(" . implode(",", $zones) . ")" : "") . " AND (payment_time=0 OR payment_time < " . time() . ")" . $sql_extra . " ORDER BY payment_time DESC";
     }
--- plugins/oio-ad-manager/routes.php
+++ plugins/oio-ad-manager/routes.php
@@ -6,7 +6,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/ads/{ad_type}', function (Request $request, Response $response, $args) {
-    $ads = new Ads($this->db, $this->logger);
+    $ads = new Ads($this->db, $this->logger, $this->get('settings'));
     $adsArr = $ads->getAds($args['ad_type']);
     $newResponse = $response->withJson(['ads'=>$adsArr]);
 
