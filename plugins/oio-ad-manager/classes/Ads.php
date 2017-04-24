94ea4aa21644015b6edde933361a2348a13fd1ce
Destin Moulton <destin.moulton@gmail.com>
2017-04-24 16:09:48 -0600

    Modified the ad query.

--- plugins/oio-ad-manager/classes/Ads.php
+++ plugins/oio-ad-manager/classes/Ads.php
@@ -6,6 +6,8 @@
  */
 
 class Ads {
+    const OIO_TABLE_PURCHASES = "ltdbsoiopub_purchases";
+
     function __construct($db, $logger){
         $this->db = $db;
         $this->logger = $logger;
@@ -15,6 +17,6 @@ class Ads {
         //The types of ads
         $types = array( 'link' => 2, 'inline' => 3, 'banner' => 5 );
 
-        $sql = "SELECT * FROM " . $this->settings->dbtable_purchases . " WHERE item_channel=" . $types[$type] . " AND item_status=1 AND payment_status=1" . ($zones ? " AND item_type IN(" . implode(",", $zones) . ")" : "") . " AND (payment_time=0 OR payment_time < " . time() . ")" . $sql_extra . " ORDER BY payment_time DESC";
+        $sql = "SELECT * FROM " . self::OIO_TABLE_PURCHASES . " WHERE item_channel=" . $types[$type] . " AND item_status=1 AND payment_status=1" . ($zones ? " AND item_type IN(" . implode(",", $zones) . ")" : "") . " AND (payment_time=0 OR payment_time < " . time() . ")" . $sql_extra . " ORDER BY payment_time DESC";
     }
 }
