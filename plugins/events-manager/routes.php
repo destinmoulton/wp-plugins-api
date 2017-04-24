1fbee7679085035809c5c895d61c501056eb8e94
Destin Moulton <destin.moulton@gmail.com>
2017-04-24 16:28:35 -0600

    Pass settings to the Events constructor.

--- plugins/events-manager/routes.php
+++ plugins/events-manager/routes.php
@@ -5,7 +5,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/events/{date}', function (Request $request, Response $response, $args) {
-    $events = new Events($this->db, $this->logger);
+    $events = new Events($this->db, $this->logger, $this->get('settings'));
     $evs = $events->getForDate($args['date']);
     $newResponse = $response->withJson(['events'=>$evs]);
 
@@ -13,7 +13,7 @@ $app->get('/events/{date}', function (Request $request, Response $response, $arg
 });
 
 $app->get('/event_debug/{event_id}', function (Request $request, Response $response, $args) {
-    $events = new Events($this->db, $this->logger);
+    $events = new Events($this->db, $this->logger, $this->get('settings'));
     $ev = $events->getSingleByID($args['event_id']);
     $newResponse = $response->getBody()->write($ev['post_content']);
 
