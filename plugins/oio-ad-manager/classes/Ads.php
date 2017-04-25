63573298ce7a801ff8aa54dedbc408511fc9ce59
Destin Moulton <destin.moulton@gmail.com>
2017-04-25 10:23:09 -0600

    Added query to the hasAlreadyClicked function.

--- plugins/oio-ad-manager/classes/Ads.php
+++ plugins/oio-ad-manager/classes/Ads.php
@@ -9,6 +9,7 @@ class Ads {
     const OIO_TABLE_PURCHASES = "oiopub_purchases";
     const OIO_TABLE_TRACKER_CLICKS = "oiopub_tracker_clicks";
     const OIO_TABLE_TRACKER_VISITS = "oiopub_tracker_visits";
+    const DUPLICATE_CLICK_TIME_LIMIT = 1800;
 
     function __construct($db, $logger, $settings){
         $this->db = $db;
@@ -31,7 +32,20 @@ class Ads {
         return $stmt->fetchAll();
     }
 
-    function checkIfClicked($purchase_id, $ip){
-        
+    function hasAlreadyClicked($pid, $client_ip){
+        $date_now = date('Y-m-d', $time_now);
+        $time_now = time();
+        $allowed_time_difference = $time_now - self::DUPLICATE_CLICK_TIME_LIMIT;
+
+        $select = $this->db->select()
+                           ->from($this->settings['db']['prefix'] . self::OIO_TABLE_TRACKER_CLICKS)
+                           ->where("pid", "=", $pid)
+                           ->where("ip", "=", $client_ip)
+                           ->where("time", ">", $allowed_time_difference);
+
+        $stmt = $select->execute();
+        $click = $stmt->fetch();
+
+        return isset($click['pid']);
     }
 }
