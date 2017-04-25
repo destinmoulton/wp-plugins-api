5ae834804cf47f750343baa9384e8c5fd7efed24
Destin Moulton <destin.moulton@gmail.com>
2017-04-25 10:12:09 -0600

    Added stub checkIfClicked function.

--- plugins/oio-ad-manager/classes/Ads.php
+++ plugins/oio-ad-manager/classes/Ads.php
@@ -6,7 +6,7 @@
  */
 
 class Ads {
-    const OIO_TABLE_PURCHASES = "ltdbsoiopub_purchases";
+    const OIO_TABLE_PURCHASES = "oiopub_purchases";
 
     function __construct($db, $logger, $settings){
         $this->db = $db;
@@ -15,21 +15,21 @@ class Ads {
     }
 
     function getAdsByType($adType){
-        //The types of ads
         $types = array( 'link' => 2, 'inline' => 3, 'banner' => 5 );
 
         $select = $this->db->select()
                            ->from($this->settings['db']['prefix'] . self::OIO_TABLE_PURCHASES)
-                           ->where("item_channel", "=", $types[$type])
+                           ->where("item_channel", "=", $types[$adType])
                            ->where("item_status", "=", 1)
                            ->where("payment_status", "=", 1)
-                           ->where("payment_time", "=", 0)
-                           ->orWhere("payment_time", "<", time())
+                           ->where("payment_time", "<", time())
                            ->orderBy("payment_time", "DESC");
 
         $stmt = $select->execute();
         return $stmt->fetchAll();
+    }
 
-        //$sql = "SELECT * FROM " . self::OIO_TABLE_PURCHASES . " WHERE item_channel=" . $types[$type] . " AND item_status=1 AND payment_status=1" . ($zones ? " AND item_type IN(" . implode(",", $zones) . ")" : "") . " AND (payment_time=0 OR payment_time < " . time() . ")" . $sql_extra . " ORDER BY payment_time DESC";
+    function checkIfClicked($purchase_id, $ip){
+
     }
 }
