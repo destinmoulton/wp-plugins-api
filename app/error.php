f037642fe5f547821ad78cd1c50c5ffd0dfc2e58
Destin Moulton <destin.moulton@gmail.com>
2017-06-05 13:46:32 -0600

    Added an error.php file to display from the custom error handler.

---
+++ app/error.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>WP Plugins API Error</title>
+
+</head>
+
+<body>
+<h1>Slim Error Encountered</h1>
+<?php
+
+echo $error;
+
+?>
+</body>
+</html>
+
