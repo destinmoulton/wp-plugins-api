b39e9876897323f1cffb7cc5772c9f9d6170b06b
Destin Moulton <destin.moulton@gmail.com>
2017-06-05 13:56:04 -0600

    Modified the response var.

--- app/init.php
+++ app/init.php
@@ -14,9 +14,9 @@ $container = $app->getContainer();
 // Custom error handler
 $container['errorHandler'] = function ($container) {
     return function ($request, $response, $exception) use ($container) {
-        return $container['response']->withStatus(500)
+        return $response->withStatus(500)
                                      ->withHeader('Content-Type', 'text/html')
-                                      ->write($exception->getMessage());
+                                     ->write($exception->getMessage());
     };
 };
 
